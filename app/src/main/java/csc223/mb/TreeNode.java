5c7c6888d62c27f088c15131c83800b3893b4e58
Madigan Brewer <madigan@Madigans-MBP.fu.campus>
2025-03-12 14:45:49 -0400

    Lab 5

--- app/src/main/java/csc223/mb/TreeNode.java
+++ app/src/main/java/csc223/mb/TreeNode.java
@@ -4,12 +4,19 @@ public class TreeNode {
     TreeNode left;
     TreeNode right;
    
-    char data;
+   
+    int data;
 
-    public TreeNode(char data){
+    public TreeNode(){
+        this.left = null;
+        this.right = null;
+        
+        
+    }
+
+    public TreeNode(int data){
         this.left = null;
         this.right = null;
         this.data = data;
-        
     }
 }
